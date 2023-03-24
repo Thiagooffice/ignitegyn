d9da20bc5dc28f1dc83ff7600517fb2da6635487
Thiago <thiagooffice2@gmail.com>
2023-03-24 14:53:30 -0300

    feat: add variant in button and add scrollView in SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base'
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from "@assets/logo.svg"
@@ -7,43 +7,56 @@ import { Button } from '@components/Button'
 
 export function SignIn() {
     return (
-        <VStack px={10} flex={1} bg="gray.700">
-            <Image
-                alt='Pessoas treinando'
-                source={BackgroundImg}
-                resizeMode="contain"
-                position="absolute"
-            />
-            <Center my={24}>
-                <LogoSvg />
-
-                <Text color="gray.100" fontSize="sm">
-                    Treine sua mente e o seu corpo
-                </Text>
-            </Center>
-
-            <Center>
-                <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
-                    Acesse sua conta
-                </Heading>
-
-                <Input
-                    placeholder='E-mail'
-                    keyboardType='email-address'
-                    autoCapitalize='none'
+        <ScrollView contentContainerStyle={{flexGrow: 1}} showsHorizontalScrollIndicator={false}>
+            <VStack px={10} flex={1} bg="gray.700">
+                <Image
+                    alt='Pessoas treinando'
+                    source={BackgroundImg}
+                    resizeMode="contain"
+                    position="absolute"
                 />
+                <Center my={24}>
+                    <LogoSvg />
 
-                <Input
-                    placeholder='Senha'
-                    secureTextEntry
-                />
+                    <Text color="gray.100" fontSize="sm">
+                        Treine sua mente e o seu corpo
+                    </Text>
+                </Center>
+
+                <Center>
+                    <Heading fontSize="xl" mb={6} fontFamily="heading" color="gray.100">
+                        Acesse sua conta
+                    </Heading>
+
+                    <Input
+                        placeholder='E-mail'
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                    />
+
+                    <Input
+                        placeholder='Senha'
+                        secureTextEntry
+                    />
+
+                    <Button
+                        title='Acessar'
+                    />
+
+                </Center>
+
+                <Center mt={24}>
+                    <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+                        Ainda não tem acesso?
+                    </Text>
+                </Center>
 
                 <Button
-                title='Acessar'
+                    title='Criar conta'
+                    variant="outline"
                 />
 
-            </Center>
-
-        </VStack>
+            </VStack>
+        </ScrollView>
     )
 }
