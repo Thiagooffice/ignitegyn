7f88fb1795afe0ae6e607277272017d808a82b75
Thiago <thiagooffice2@gmail.com>
2023-03-23 23:19:01 -0300

    feat: add native base and config default theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from "native-base";
+import { Loading } from '@components/Loading';
+
+import {THEME} from './src/theme'
 
 export default function App() {
 
@@ -7,13 +11,15 @@ export default function App() {
 
   return (
     
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024"}}>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
       barStyle="light-content"
       backgroundColor="transparent"
       translucent
       />
-      {fontsLoaded ? <Text>Hello WOrld</Text> : <View/>}
-    </View>
+      {
+        fontsLoaded ? <View/> : <Loading/>
+      }
+    </NativeBaseProvider>
   );
 }
