f2e52270e63d4a5ba2f4fe13df224c8b24030ea5
Thiago <thiagooffice2@gmail.com>
2023-03-27 15:55:17 -0300

    feat: creating group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,28 @@
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import {VStack, Text} from 'native-base'
+import { VStack, Text, HStack } from 'native-base'
+import { useState } from 'react'
 
-export function Home(){
-    return(
+export function Home() {
+
+    const [groupSelected, setGroupSelected] = useState("costa")
+
+    return (
         <VStack flex={1}>
-                <HomeHeader/>
+            <HomeHeader />
+
+            <HStack>
+                <Group
+                    onPress={() => setGroupSelected("costa")}
+                    name="costa"
+                    isActive={groupSelected === "costa"}
+                />
+                <Group
+                    onPress={() => setGroupSelected("ombro")}
+                    name="ombro"
+                    isActive={groupSelected === "ombro"}
+                />
+            </HStack>
         </VStack>
     )
 }
