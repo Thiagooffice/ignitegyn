0e09b3ccfe14483e3a9ef41f8ce1ca883a73c799
Thiago <thiagooffice2@gmail.com>
2023-03-27 19:03:42 -0300

    feat: creating history card

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,44 @@
-import {Center, Text} from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
-export function Profile(){
-    return(
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
-        </Center>
+export function Profile() {
+    const [photoIsLoading, setphotoIsLoading] = useState(false)
+
+    const PHOTO_SIZE = 33
+
+    return (
+        <VStack flex={1}>
+            <ScreenHeader title='Perfil' />
+
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ?
+                            <Skeleton
+                                w={PHOTO_SIZE}
+                                h={PHOTO_SIZE}
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                            />
+                            :
+                            <UserPhoto
+                                source={{ uri: "https://github.com/thiagooffice.png" }}
+                                alt="Foto do usuário"
+                                size={PHOTO_SIZE}
+                            />
+                    }
+
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }
